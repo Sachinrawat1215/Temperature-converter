c704e590de4a0ec67f592e632113fea202ab4fce
Sachinrawat1215 <sachinrawat0853@gmail.com>
2021-06-11 11:30:23 +0530

    decimal fixed to 4 digit...

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ let fah = document.getElementById("boxtwo");
 cel.addEventListener("input", function(){
     let celsius = this.value;
     let fahrenheit = (celsius * 9 / 5) + 32;
+
+    function header(){
+        
+    }
+    
+    if(!Number.isInteger(fahrenheit)){
+        fahrenheit = fahrenheit.toFixed(4);
+    }
+
     fah.value = fahrenheit;
  
     function show(){
         let formula = document.querySelector(".main-formula");
         formula.innerHTML = `<p>(${celsius}°C × 9/5) + 32 = ${fahrenheit}°F</p>`
-        console.log(formula);
     }
     show();
 });
@@ -19,12 +27,16 @@ cel.addEventListener("input", function(){
 fah.addEventListener("input", function(){
     let fahrenheit = this.value;
     let celsius = (fahrenheit - 32) * 5 / 9;
+    
+    if(!Number.isInteger(celsius)){
+        celsius = celsius.toFixed(4);
+    }
+
     cel.value = celsius;
 
     function show(){
         let formula = document.querySelector(".main-formula");
         formula.innerHTML = `<p>(${fahrenheit}°F − 32) × 5/9 = ${celsius}°C</p>`
-        console.log(formula);
     }
     show();
 });
