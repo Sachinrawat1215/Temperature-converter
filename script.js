d52cb2b1d4ebdd93c3998bfe2123170d4ef21cc0
Sachinrawat1215 <sachinrawat0853@gmail.com>
2021-06-11 10:46:13 +0530

    showing formula task completed

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ cel.addEventListener("input", function(){
     let celsius = this.value;
     let fahrenheit = (celsius * 9 / 5) + 32;
     fah.value = fahrenheit;
+ 
+    function show(){
+        let formula = document.querySelector(".main-formula");
+        formula.innerHTML = `<p>(${celsius}°C × 9/5) + 32 = ${fahrenheit}°F</p>`
+        console.log(formula);
+    }
+    show();
 });
 
 fah.addEventListener("input", function(){
     let fahrenheit = this.value;
     let celsius = (fahrenheit - 32) * 5 / 9;
     cel.value = celsius;
+
+    function show(){
+        let formula = document.querySelector(".main-formula");
+        formula.innerHTML = `<p>(${fahrenheit}°F − 32) × 5/9 = ${celsius}°C</p>`
+        console.log(formula);
+    }
+    show();
 });
